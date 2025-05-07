cef37fb554de315d32ab12b2d7aee3fea3c2186b
Partha Protim Gogoi <176602154+g-partha@users.noreply.github.com>
2025-05-07 20:54:30 +0530

    Initial configuration

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import { recoverTodosFromLocalStorage } from "./todos";
-import { recoverProjectsFromLocalStorage } from "./projects";
-import { showTodosList, showProjectList } from "./gui";
 import "./style.css";
-
-recoverTodosFromLocalStorage();
-recoverProjectsFromLocalStorage();
-showTodosList("all");
-showProjectList();
